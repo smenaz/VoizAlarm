f9042f487ec0f3366685818cb21fac4ca72b923b
w.lopezm <w.lopezm@uniandes.edu.co>
2025-03-13 16:29:30 -0500

    Versión WEB Sonido por eventos-Wlopezm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HomeComponent } from './components/home/home.component';
 import { AlarmasUbicacionListComponent } from './alarmas-ubicacion/alarmas-ubicacion-list/alarmas-ubicacion-list.component';
+import { SonidoEventosListComponent } from './sonido-eventos/sonido-eventos-list/sonido-eventos-list.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'test', component: AlarmasUbicacionListComponent}, // Ruta de prueb
   {path: 'alarmas', loadChildren: () => import('./alarmas-ubicacion/alarmas-ubicacion.module').then(m => m.AlarmasUbicacionModule)},
+  {path: 'sonidoeventos', loadChildren: () => import('./sonido-eventos/sonido-eventos.module').then(m => m.SonidoEventosModule)},
   { path: '**', component: PageNotFoundComponent } 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShareModule } from './share/share.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Importa los módulos específicos de Carbon Components Angular
 import { TableModule, ButtonModule, IconModule, GridModule, PaginationModule } from 'carbon-components-angular';
 import { AlarmasUbicacionModule } from './alarmas-ubicacion/alarmas-ubicacion.module';
+import { SonidoEventosModule } from './sonido-eventos/sonido-eventos.module';
 
 
 @NgModule({
@@ -23,7 +27,12 @@ import { AlarmasUbicacionModule } from './alarmas-ubicacion/alarmas-ubicacion.mo
     GridModule,
     PaginationModule,
     ShareModule,
-    AlarmasUbicacionModule
+    AlarmasUbicacionModule,
+    SonidoEventosModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
